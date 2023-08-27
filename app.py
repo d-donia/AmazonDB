6400c72708124e84e940876b7476f03e826117c4
d-donia <d.donia@studenti.unisa.it>
2023-08-27 21:18:22 +0200

    Addition insert page

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():  # put application's code here
     return render_template("index.html")
 
+@app.route('/insert')
+def insert():  # put application's code here
+    return render_template("insert.html")
+
+@app.route('/delete')
+def delete():  # put application's code here
+    return render_template("delete.html")
+
+@app.route('/modify')
+def modify():  # put application's code here
+    return render_template("modify.html")
+
+@app.route('/search')
+def search():  # put application's code here
+    return render_template("search.html")
+
 
 if __name__ == '__main__':
     app.run()
